ead2d5ef586732c888f2e6d2fcdbe002208ce693
maksk <wgagaqwt1@gmail.com>
2025-05-10 20:44:42 +0200

    First commit

---
+++ Jewbee/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program{
+    static void Main(string[] args){
+        
+    }
+    public static void Process()
+    {
+        NoobCoders.ProcessData();
+    }
+}
+
+class NoobCoders{
+    public static void ProcessData(){
+        throw new Exception(InvalidOperationException);
+    }
+}
