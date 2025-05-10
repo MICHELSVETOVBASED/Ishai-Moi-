3479ca2ba228980944e5f6980cd038768a341abf
maksk <wgagaqwt1@gmail.com>
2025-05-10 21:41:21 +0200

    New local machine

--- Jewbee/Program.cs
+++ Jewbee/Program.cs
@@ -1,15 +1,24 @@
-﻿class Program{
+﻿using System.Net;
+
+class Program{
     static void Main(string[] args){
-        
+        Process();
     }
     public static void Process()
     {
-        NoobCoders.ProcessData();
+        try{
+            NoobCoders.ProcessData();
+        }
+        catch (InvalidOperationException e){
+            Console.WriteLine(e);
+        }
     }
 }
 
 class NoobCoders{
-    public static void ProcessData(){
-        throw new Exception(InvalidOperationException);
+    public static int ProcessData(){
+        Queue<int> numbers = new Queue<int>();
+        return numbers.Dequeue();
+
     }
 }
