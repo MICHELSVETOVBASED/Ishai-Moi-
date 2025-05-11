c3f88e7367ca82fbb5a12234ef772aa865e39b20
maksk <wgagaqwt1@gmail.com>
2025-05-11 23:06:59 +0200

    Test 2

--- Jewbee/Program.cs
+++ Jewbee/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
 
 class Program{
     static void Main(string[] args){
@@ -9,8 +11,23 @@ class Program{
         try{
             NoobCoders.ProcessData();
         }
-        catch (InvalidOperationException e){
-            Console.WriteLine(e);
+        catch (OperationCanceledException)
+        {
+            throw;
+        }
+        catch (InvalidOperationException e) when (e.Message.Contains("This error should not be re-thrown"))
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (InvalidOperationException e)
+        {
+            Console.WriteLine(e.Message);
+            ExceptionDispatchInfo.Capture(e).Throw();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            ExceptionDispatchInfo.Capture(e).Throw();
         }
     }
 }
@@ -19,6 +36,5 @@ class NoobCoders{
     public static int ProcessData(){
         Queue<int> numbers = new Queue<int>();
         return numbers.Dequeue();
-
     }
 }
