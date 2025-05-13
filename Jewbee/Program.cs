56211caa864c1cd5725a4a48c4afb31c729967ca
maksk <wgagaqwt1@gmail.com>
2025-05-13 23:52:39 +0200

    task4

--- Jewbee/Program.cs
+++ Jewbee/Program.cs
@@ -2,26 +2,47 @@
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
 using System.IO;
+using System.Runtime.InteropServices;
 
+
+namespace Jewbee;
 class Program{
     static void Main(string[] args){
         
     }
 
-    public class MainClass
-    {    
-        private static readonly string[] filmGenres =
-        {
-            "Детектив", "Комедия", "Триллер",
-            "Научная фантастика", "Документальный",
-            "Ужасы", "Фэнтези", "Боевик",
-            "Мультфильм", "Приключения",
-            "Криминал", "Мюзикл","Проверьте корректность ввода"
-        };
+    public static NoobCodersUser CreateUser(string name, string email)
+    {
+        // доработайте этот метод, добавив валидацию входных данных,
+        // обработку и логирование ошибок
+        ArgumentNullException.ThrowIfNull(name);
+        ArgumentNullException.ThrowIfNull(email);
+        var log = new string[]{ name, email };
+        if (log.Contains("")){
+            throw new ArgumentException("Value cannot be empty");
+        }
+        if (!email.Contains('@')){
+            throw new ArgumentException("Email should contain '@'");
+        }
 
-        public static void PrintGenre(){
 
-            Console.WriteLine(filmGenres[int.TryParse(Console.ReadLine(), out int result) && result <12 ? result : 12]);
+        var dbContext = NoobCodersDatabase.CreateContext();
+        var user = dbContext.CreateUser(name, email);
+        return user;
+    }
+    
+}
+
+public class NoobCodersUser{
+}
+public class NoobCodersDatabase{
+    public static  DbContext CreateContext(){
+        return new DbContext();
+    }
+
+    public class DbContext{
+        public NoobCodersUser CreateUser(string name,string email){
+            return new NoobCodersUser();
         }
     }
 }
