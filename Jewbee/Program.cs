72fc857e8c9a2cc4b20b8bf4daa43be7620b6061
maksk <wgagaqwt1@gmail.com>
2025-05-14 22:25:05 +0200

    Test4?

--- Jewbee/Program.cs
+++ Jewbee/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
 using System.IO;
 using System.Runtime.InteropServices;
-
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Server;
 
 namespace Jewbee;
 class Program{
@@ -11,8 +13,7 @@ class Program{
         
     }
 
-    public static NoobCodersUser CreateUser(string name, string email)
-    {
+    public static NoobCodersUser CreateUser(string name, string email){
         // доработайте этот метод, добавив валидацию входных данных,
         // обработку и логирование ошибок
         ArgumentNullException.ThrowIfNull(name);
@@ -21,28 +22,53 @@ class Program{
         if (log.Contains("")){
             throw new ArgumentException("Value cannot be empty");
         }
+
         if (!email.Contains('@')){
             throw new ArgumentException("Email should contain '@'");
         }
 
-
         var dbContext = NoobCodersDatabase.CreateContext();
-        var user = dbContext.CreateUser(name, email);
+        NoobCodersUser user = null;
+        try{
+            user = dbContext.CreateUser(name, email);
+        }
+        catch (SqlException ex){
+            if (ex.Message == "Cannot insert duplicated value"){
+                throw new InvalidOperationException("The user already added");
+            }
+
+            throw; // пробрасываем другие исключения выше
+        }
+        catch (NullReferenceException ex){
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        catch (UnauthorizedAccessException ex){
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        finally{
+            dbContext.Dispose();
+        }
         return user;
     }
-    
 }
 
 public class NoobCodersUser{
 }
+
 public class NoobCodersDatabase{
-    public static  DbContext CreateContext(){
+    public static DbContext CreateContext(){
         return new DbContext();
     }
 
-    public class DbContext{
+    public class DbContext : IDisposable{
         public NoobCodersUser CreateUser(string name,string email){
             return new NoobCodersUser();
+        }
+
+        public void Dispose(){
+            // Реализация Dispose
         }
     }
 }
